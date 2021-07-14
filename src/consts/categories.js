331da5965420ca0dfb98cda2b5927d5c16c30f11
Alireza <Alireza691111@gmail.com>
2021-07-14 05:12:02 -0700

    Native Files Added

---
+++ src/consts/categories.js
@@ -0,0 +1,8 @@
+const categories = [
+  {id: '1', name: 'پیتزا', image: require('../assets/catergories/pizza.png')},
+  {id: '2', name: 'برگر', image: require('../assets/catergories/burger.png')},
+  {id: '3', name: 'سوشی', image: require('../assets/catergories/sushi.png')},
+  {id: '4', name: 'سالاد', image: require('../assets/catergories/salad.png')},
+];
+
+export default categories;
---
+++ src/consts/foods.js
@@ -0,0 +1,36 @@
+const foods = [
+  {
+    id: '1',
+    name: 'پیتزا گوشتی',
+    ingredients: 'پیتزا مخلوط',
+    count:0,
+    price: '8.30',
+    image: require('../assets/meatPizza.png'),
+  },
+  {
+    id: '2',
+    name: 'پیتزا پنیری',
+    ingredients: 'پیتزا پنیری',
+    count:0,
+    price: '7.10',
+    image: require('../assets/cheesePizza.png'),
+  },
+  {
+    id: '3',
+    name: 'چیکن برگر',
+    ingredients: 'مرغ سرخ شده',
+    count:0,
+    price: '5.10',
+    image: require('../assets/chickenBurger.png'),
+  },
+  {
+    id: '4',
+    name: 'سوشی',
+    ingredients: 'گوشت  ماهی سالمون',
+    count:0,
+    price: '9.55',
+    image: require('../assets/sushiMakizushi.png'),
+  },
+];
+
+export default foods;
